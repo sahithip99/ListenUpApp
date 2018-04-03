b1f0a492ea21d0791370e23bcda9da6d1f2f0f45
Sahithi <sp3429@drexel.edu>
2018-04-03 16:30:57 -0400

    Handling errors with authentication...completed!

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -18,6 +18,8 @@ import{AngularFireDatabase} from 'angularfire2/database';
 })
 export class RegisterPage {
   user = {} as User;
+  errorMessage = ""
+
   constructor(private afAuth: AngularFireAuth,
     public navCtrl: NavController, public navParams: NavParams, public afData: AngularFireDatabase) {
   }
@@ -25,6 +27,18 @@ export class RegisterPage {
 
 
   async register(user: User){
+    if(user.username == null){
+         this.errorMessage = "Please provide a valid username"
+         return
+    }
+    if(user.firstname == null){
+         this.errorMessage = "Please provide a valid firstname"
+         return
+    }
+    if(user.lastname == null){
+         this.errorMessage = "Please provide a valid last name"
+         return
+    }
     try{
     const result = await this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password);
     var userObj = {
@@ -38,11 +52,27 @@ export class RegisterPage {
     };
     this.afData.database.ref("users").child(userObj.id).update(userObj).then(success =>{
       console.log("hooray");
+      console.log(userObj);
+
     })
-    console.log(userObj);
       }
     catch(e){
       console.error(e);
+      if(e.code == "auth/argument-error"){
+           this.errorMessage = "Please fill in required boxes"
+         }
+      if(e.code == "auth/invalid-email"){
+           this.errorMessage = "Please provide valid email"
+      }
+      if(e.code == "auth/weak-password"){
+           this.errorMessage = "Password too weak. Passwords must be six characters long or more"
+      }
+      if(e.code == "auth/email-already-in-use"){
+           this.errorMessage = "Email already in use. Please use a different email"
+      }
+
+
+
     }
   }
 }
