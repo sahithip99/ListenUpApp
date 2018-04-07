e7b4a2c65c4351eac9d16f45661b3a01a9b3dab9
LAPTOP-QP4LB6MT\Allie <allielinhdo@gmail.com>
2018-04-07 15:04:06 -0400

    register check done

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -17,16 +17,63 @@ import{AngularFireDatabase} from 'angularfire2/database';
   templateUrl: 'register.html',
 })
 export class RegisterPage {
-  user = {} as User;
+
+  //VARIABLES TO BE USED
+  user:any = {
+    email: "",
+    password : "",
+    username: "",
+    firstname: "",
+    lastname: "",
+  }
+
+
+  //CONSTRUCTOR: all the things that have to be loaded to run the page
   constructor(private afAuth: AngularFireAuth,
     public navCtrl: NavController, public navParams: NavParams, public afData: AngularFireDatabase) {
   }
 
 
+//FUNCTIONS
 
-  async register(user: User){
+
+
+registerPeople(){
+  function checkEmpty(user){
+    for(var i in user){
+      if(user[i] == '' || user[i] == " " || !(user[i])){
+        return false;
+      }
+    }
+    return true;
+  }
+
+
+  if(checkEmpty(this.user)){
+    //register the d00d
+    this.afAuth.auth.createUserWithEmailAndPassword(this.user.email,this.user.password).then(success => {
+    //woooooo were logged in
+    this.afData.database.ref('users').child(success.uid).update({
+      id: success.uid,
+      firstname: this.user.firstname,
+      lastname: this.user.lastname,
+      email: this.user.email
+    }).then(winning => {
+      //donald trump
+      console.log("all is done");
+      return;
+    })
+  })
+  }
+  else{
+    console.log("something happend! fix itttttt");
+    return;
+  }
+
+}
+  /*async register(user: User){ //async: all at once, 2+ simultaneously
     try{
-    const result = await this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password);
+    const result = await this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password); //because of async, has to be done
     var userObj = {
       email: user.email,
       password : user.password,
@@ -37,12 +84,19 @@ export class RegisterPage {
       login: false
     };
     this.afData.database.ref("users").child(userObj.id).update(userObj).then(success =>{
-      console.log("hooray");
+      for (var i in userObj){
+        if (userObj[i]=''){
+          console.log(i + "field has not been filled out.");
+          console.error(e);
+        }
+        else {console.log("hooray");}
+      }
+
     })
     console.log(userObj);
       }
     catch(e){
       console.error(e);
     }
-  }
+  }*/
 }
