70c336e225da17bc629ddb0e2fc13869b083ba97
lixinwei123 <lixinwei123@yahoo.com>
2018-05-16 23:29:57 -0400

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,44 +20,58 @@ export class MyApp {
   usrInfo: any;
   constructor(platform: Platform, statusBar: StatusBar,
     splashScreen: SplashScreen, private afAuth: AngularFireAuth, private uInfo:UserInfoProvider) {
-
+     this.loadUserInfo();
     platform.ready().then(() => {
        this.afAuth.auth.onAuthStateChanged(user => {
-         if(user != undefined){
-         this.uInfo.setUserInfo(user).then(success =>{
-           this.loadUserInfo(user);
-         });
-       }
+         if(user){
+           this.uInfo.setUserInfo(user)
+           this.loadUserInfo();
+         }
+         else{
+            this.rootPage = LoginPage;
+         }
+         // if(user != null){
+         //.then(success =>{
+
+           console.log("auth state",user);
+         //});
+       // }
       });
       statusBar.styleDefault();
       splashScreen.hide();
 
     });
   }
-loadUserInfo(user){
+loadUserInfo(){
     this.usrInfo = this.uInfo.getUserInfo();
     // this.uInfo.setUsers();
-    this.uInfo.setPhoto(this.usrInfo.id);
-    if (this.usrInfo == undefined || this.usrInfo == null || this.uInfo.getPhoto() == undefined || this.uInfo.getPhoto() == null){
+    if(this.usrInfo){
+           this.uInfo.setPhoto(this.usrInfo.id);
+    }
+
+    // if (user == null){
+    //     //user logged out
+    //       this.rootPage = LoginPage;
+    //  }
+     if (this.usrInfo == undefined || this.uInfo.getPhoto() == undefined ){
       setTimeout(() => {
-        this.loadUserInfo(user);
+        this.loadUserInfo();
       },1000);
     }
-    else{
-       if(user){
+    // else{
+       // if(user){
           //user logged in
+        else{
           if(this.usrInfo.username){
               this.rootPage = TabsPage;
           }
-          else{
+                    else {
             this.rootPage = CreateuserPage;
           }
         }
-        else{
-        //user logged out
-          this.rootPage = LoginPage;
-        };
-    }
-  }
+          // }
+   
+  // }
 
+}
 }
